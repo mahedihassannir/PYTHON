0ffe1f9a3e88b20b4275dadb27aaedac3359fac9
mahedihasssannir <mahedinir34678@gmail.com>
2024-10-26 21:05:49 +0600

    done

---
+++ full overview/overview.py
@@ -0,0 +1,19 @@
+# person = {
+#     "name": "Mahedi Hassan",
+#     "age": 17,
+# }
+
+# for x,y in person.items():
+#     print(x,y)
+
+
+# how to create lamda function
+
+# x = lambda a: print(a)
+
+# x(20)
+
+
+import os
+
+os.remove("new.txt")
